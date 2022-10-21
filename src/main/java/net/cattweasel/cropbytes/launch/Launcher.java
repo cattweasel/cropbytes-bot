44309abeb7758781e161baf3ff86d6c6190e7eed
bwesp <bwesp@sickmachine>
2022-10-21 08:31:07 +0200

    Auto build standalone jar with dependencies

--- src/main/java/net/cattweasel/cropbytes/launch/Launcher.java
+++ src/main/java/net/cattweasel/cropbytes/launch/Launcher.java
@@ -1,5 +1,7 @@
 package net.cattweasel.cropbytes.launch;
 
+import org.apache.log4j.Logger;
+
 import com.pengrad.telegrambot.TelegramBot;
 
 import net.cattweasel.cropbytes.task.AlertNotificationTask;
@@ -8,6 +10,8 @@ import net.cattweasel.cropbytes.telegram.UpdateListener;
 
 public class Launcher {
 
+	private static final Logger LOG = Logger.getLogger(Launcher.class);
+	
 	public static void main(String[] args) {
 		
 		String telegramBotApiKey = System.getProperty("TELEGRAM_BOT_API_KEY");
@@ -16,12 +20,16 @@ public class Launcher {
 			System.exit(1);
 		}
 		
+		LOG.debug("Setting up telegram bot instance..");
 		TelegramBot bot = new TelegramBot(telegramBotApiKey);
 		bot.setUpdatesListener(new UpdateListener(bot));
 		
+		LOG.debug("Setting up market data refresh task..");
 		new MarketDataRefreshTask().start();
 		
+		LOG.debug("Setting up alert notification task..");
 		new AlertNotificationTask(bot).start();
 		
+		LOG.debug("Bot is now up and running!");
 	}
 }
